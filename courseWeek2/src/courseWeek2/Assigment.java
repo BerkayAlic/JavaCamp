d26253de1929fa69116a337bd58f0b47371a90fa
berkay <berkayalic2@gmail.com>
2021-05-02 00:46:32 +0300

    a

--- courseWeek2/src/courseWeek2/Assigment.java
+++ courseWeek2/src/courseWeek2/Assigment.java
@@ -5,7 +5,7 @@ public class Assigment {
 	public static void main(String[] args) {
 		Course course1 = new Course(1,"Engin Demirog","C# + Angular",95);
 		Course course2 = new Course(2,"Engin Demirog","Java + React",32);
-		Course course3 = new Course(3,"Engin Demirog","Jquery",12);
+		Course course3 = new Course(3,"Engin Demirog","Jquery",15);
 		
 		Course[] courses = {course1, course2};
 		
